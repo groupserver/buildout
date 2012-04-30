1db7eaceb83a3667bf2566b5f3c76f3aa672b797
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-01 09:39:43 +1000

    Trying to get buildout to work.

--- gs_install_ubuntu.sh
+++ gs_install_ubuntu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 . read_ini.sh
-
 read_ini config.cfg config --prefix CFG
 
-# install packages
+echo "Installing dependencies"
+echo
 sudo apt-get install -y python python-virtualenv python-dev build-essential postgresql libpq-dev libxslt1-dev libxml2-dev python-libxslt1 swaks
 
+echo
 echo "Testing mail setup..."
 echo
 for tmail in ${CFG__config__admin_email} ${CFG__config__user_email}
@@ -18,23 +19,19 @@ do
     fi
 
     if [ $? -eq 0 ]; then
-        echo "Expect an email at ${tmail} from ${CFG__config__support_email}!"
+        echo "Mail setup works. Expect an email at ${tmail}"
+        echo "from ${CFG__config__support_email}."
+        echo
     else
         echo
-        echo "There was a problem with the mail configuration. This must be fixed before proceeding with the installation."
+        echo "There was a problem with the mail configuration. This "
+        echo "must be fixed before proceeding with the installation."
         exit
     fi
 done
 
-virtualenv --no-site-packages . 
-
-# setup the environment
-. ./bin/activate
-
-# fetch build here
-pip install zc.buildout==1.5.2
-
 # initialise PostgreSQL database
+echo "Setting up the databases"
 sudo su -l -c"createuser -D -S -R -l ${CFG__config__pgsql_user}" postgres
 sudo su -l -c"createuser -D -S -R -l ${CFG__config__relstorage_user}" postgres
 
@@ -47,18 +44,31 @@ sudo su -l -c"echo \"alter user ${CFG__config__relstorage_user} with encrypted p
 sudo su -l -c"echo \"grant all privileges on database ${CFG__config__pgsql_dbname} to ${CFG__config__pgsql_user};\" | psql" postgres
 sudo su -l -c"echo \"grant all privileges on database ${CFG__config__relstorage_dbname} to ${CFG__config__relstorage_user};\" | psql" postgres
 
+# This is not the work of angels, but...
 echo "${CFG__config__pgsql_host}:${CFG__config__pgsql_port}:${CFG__config__pgsql_dbname}:${CFG__config__pgsql_user}:${CFG__config__pgsql_password}" > pgpass-tmp
 chmod 600 pgpass-tmp
-
 export PGPASSFILE=pgpass-tmp
-
+# It works.
 createlang -U${CFG__config__pgsql_user} -h${CFG__config__pgsql_host} -p${CFG__config__pgsql_port} plpgsql ${CFG__config__pgsql_dbname}
+echo
+echo "Databases created"
+echo
 
-# Update distribute
-./bin/easy_install --upgrade distribute
+echo "Setting up Python"
+echo
+# Create the Python environment
+virtualenv --no-site-packages . 
+. ./bin/activate
+# Fetch the system that builds GroupServer
+pip install zc.buildout==1.5.2
+echo
+echo "Python setup complete."
 
-# Start the buildout
+echo
+echo "Installing GroupServer"
+echo
 buildout -vN
+# Buildout has its own "completed" message.
 
 rm pgpass-tmp
 
