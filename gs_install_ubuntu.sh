022bcb1928ebbee6cdc5b110a2c47fe4e5a228e7
Michael JasonSmith <mpj17@onlinegroups.net>
2014-05-27 10:47:56 -0700

    Removing the user email-address from the install script.

--- gs_install_ubuntu.sh
+++ gs_install_ubuntu.sh
@@ -10,28 +10,24 @@ sudo apt-get install -y sed python python-virtualenv python-dev build-essential 
 echo
 echo "Testing mail setup..."
 echo
-for tmail in ${CFG__config__admin_email} ${CFG__config__user_email}
-do
-    if [ "${CFG__config__smtp_user}" != "" ]; then
-        swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${tmail} --auth-user ${CFG__config__smtp_user}  --auth-pass ${CFG__config__smtp_password}
-    else
-        swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${tmail}
-    fi
-
-    if [ $? -eq 0 ]; then
-        echo "Mail setup works. Expect an email at ${tmail}"
-        echo "from ${CFG__config__support_email}."
-        echo
-    else
-        echo
-        echo
-        echo "There was a problem with the mail configuration. Either install"
-        echo "Postfix:"
-        echo "    sudo apt-get install postfix"
-        echo "Or specify a remote SMTP server in the 'config.cfg' file."
-        exit
-    fi
-done
+if [ "${CFG__config__smtp_user}" != "" ]; then
+    swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${CFG__config__admin_email} --auth-user ${CFG__config__smtp_user}  --auth-pass ${CFG__config__smtp_password}
+else
+    swaks -S -s ${CFG__config__smtp_host} -p ${CFG__config__smtp_port} --from ${CFG__config__support_email} --to ${CFG__config__admin_email}
+fi
+if [ $? -eq 0 ]; then
+    echo "Mail setup works. Expect an email at ${CFG__config__admin_email}"
+    echo "from ${CFG__config__support_email}."
+    echo
+else
+    echo
+    echo
+    echo "There was a problem with the mail configuration. Either install"
+    echo "Postfix:"
+    echo "    sudo apt-get install postfix"
+    echo "Or specify a remote SMTP server in the 'config.cfg' file."
+    exit
+fi
 
 echo
 echo "Testing the network..."
@@ -47,7 +43,9 @@ else
 fi
 
 # initialise PostgreSQL database
+echo
 echo "Setting up the databases"
+echo
 sudo su -l -c"createuser -p ${CFG__config__pgsql_port} -D -S -R -l ${CFG__config__pgsql_user}" postgres
 sudo su -l -c"createuser -p ${CFG__config__relstorage_port}-D -S -R -l ${CFG__config__relstorage_user}" postgres
 
@@ -76,7 +74,7 @@ fi
 echo "${CFG__config__pgsql_host}:${CFG__config__pgsql_port}:${CFG__config__pgsql_dbname}:${CFG__config__pgsql_user}:${CFG__config__pgsql_password}" > pgpass-tmp
 chmod 600 pgpass-tmp
 export PGPASSFILE=pgpass-tmp
-# It works.
+# ...it works.
 createlang -U${CFG__config__pgsql_user} -h${CFG__config__pgsql_host} -p${CFG__config__pgsql_port} plpgsql ${CFG__config__pgsql_dbname}
 echo
 echo "Databases created"
