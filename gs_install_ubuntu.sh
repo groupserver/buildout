89ca2d504da2bb07a609ead91c602894b60ca991
Michael JasonSmith <mpj17@onlinegroups.net>
2015-12-14 15:52:33 -0800

    Adding a Copyright block

--- gs_install_ubuntu.sh
+++ gs_install_ubuntu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+##############################################################################
+#
+# Copyright © 2012, 2014, 2015 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 
 # First ensure we are *NOT* *ROOT*
 if [[ $(whoami) == 'root' ]]
